41434f51f798512692f53ae982d71f84ef8866e2
muhammadfauzulhanif16 <muhammadfauzulhanif16@outlook.com>
2022-02-19 00:26:52 +0700

    create loading component

---
+++ client/v2/src/components/common/Loading/index.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+import { Icon } from "../Icon";
+
+export const Loading: FC<{}> = () => {
+  return (
+    <Icon
+      icon="loader-circle"
+      size="lg"
+      title="Please wait..."
+      className="absolute top-0 h-screen right-0 left-0 flex justify-center text-slate-300 dark:text-slate-600"
+      animation="spin"
+    />
+  );
+};
