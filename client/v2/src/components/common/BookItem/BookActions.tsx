d6983bee76e9cd2655bcd81df9b945e41837ef82
muhammadfauzulhanif16 <muhammadfauzulhanif16@outlook.com>
2022-02-19 00:25:13 +0700

    create book item component

---
+++ client/v2/src/components/common/BookItem/BookActions.tsx
@@ -0,0 +1,19 @@
+interface Type {
+  icon: string;
+  bgColor: string;
+}
+
+export const BookActions: Type[] = [
+  {
+    icon: "book",
+    bgColor: "sky-300",
+  },
+  {
+    icon: "edit",
+    bgColor: "amber-300",
+  },
+  {
+    icon: "trash",
+    bgColor: "rose-300",
+  },
+];
