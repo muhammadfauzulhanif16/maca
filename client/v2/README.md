ba92ca11b1a63b1cfad5b7e38988328f80db5cad
muhammadfauzulhanif16 <muhammadfauzulhanif16@outlook.com>
2022-02-19 14:36:39 +0700

    reinstall & remake

--- client/v2/README.md
+++
@@ -1,4 +0,0 @@
-# Maca v2
-- #### Tech Stack : HTML, CSS / SCSS, Tailwind CSS, JavaScript, TypeScript, React JS, Redux JS, Axios, Golang, MySQL 
-- #### Preview    : https://maca-v2.vercel.app
-- #### GitHub     : https://github.com/muhammadfauzulhanif16/maca/tree/v2/client/v2
--- client/v2/src/index.tsx
+++ client/v2/src/index.tsx
@@ -2,7 +2,7 @@ import { render } from "react-dom";
 import { StrictMode } from "react";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import "./index.scss";
+import "./index.css";
 import App from "./App";
 import { store } from "./store";
 import reportWebVitals from "./reportWebVitals";
