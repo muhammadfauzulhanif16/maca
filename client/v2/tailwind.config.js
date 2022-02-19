4252a20da42b0ef093b2cf477d9c8e5c44f9aad5
muhammadfauzulhanif16 <muhammadfauzulhanif16@outlook.com>
2022-02-19 07:42:31 +0700

    add dark mode & form config

--- client/v2/tailwind.config.js
+++ client/v2/tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
+  darkMode: "class",
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/forms")({
+      strategy: "class",
+    }),
+  ],
 };
