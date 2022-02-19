80a9a5ebcc7e39f864caa93ee3312cc0b8d845b1
muhammadfauzulhanif16 <muhammadfauzulhanif16@outlook.com>
2022-02-19 07:44:22 +0700

    add routers

--- client/v2/src/App.tsx
+++ client/v2/src/App.tsx
@@ -1,7 +1,15 @@
 import { FC } from "react";
+import { Routes, Route } from "react-router-dom";
+import { Routers } from "./Routers";
 
-const App: FC<{}> = (): JSX.Element => {
-  return <>Hello</>;
+const App: FC<{}> = () => {
+  return (
+    <Routes>
+      {Routers.map(({ element, path }, id: number) => (
+        <Route key={id} path={path} element={element} />
+      ))}
+    </Routes>
+  );
 };
 
 export default App;
