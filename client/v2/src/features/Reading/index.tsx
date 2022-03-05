19e12365d00c5304f0c8f18b4fa2a538b593dc9c
muhammadfauzulhanif16 <muhammadfauzulhanif16@outlook.com>
2022-03-05 20:01:32 +0700

    add overflow x & y

--- client/v2/src/features/Reading/index.tsx
+++ client/v2/src/features/Reading/index.tsx
@@ -19,31 +19,33 @@ export const ReadingComponent: FC<{}> = (): JSX.Element => {
 
   return (
     <Layout titlePage="Reading">
-      <table className="w-full divide-y divide-zinc-100 dark:divide-zinc-800">
-        <thead className="w-full">
-          <tr>
-            {Field.map((title, id) => (
-              <th
-                key={id}
-                className="px-6 py-4 text-left text-xs font-medium text-zinc-500 uppercase "
-              >
-                {title}
-              </th>
-            ))}
-          </tr>
-        </thead>
-
-        <tbody className="divide-y text-zinc-900 dark:text-zinc-50 divide-zinc-100 dark:divide-zinc-800 w-full overflow-scroll">
-          {reading.map(({ title, author, year }: any, id: number) => (
-            <tr key={id}>
-              <td className="px-6 py-4 whitespace-nowrap text-sm">{id + 1}</td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm">{title}</td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm">{author}</td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm">{year}</td>
+      <div className="overflow-x-auto">
+        <table className="w-full divide-y divide-zinc-100 dark:divide-zinc-800">
+          <thead className="w-full">
+            <tr>
+              {Field.map((title, id) => (
+                <th
+                  key={id}
+                  className="px-6 py-4 text-left text-xs font-medium text-zinc-500 uppercase "
+                >
+                  {title}
+                </th>
+              ))}
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+
+          <tbody className="divide-y text-zinc-900 dark:text-zinc-50 divide-zinc-100 dark:divide-zinc-800 w-full overflow-y-auto">
+            {reading.map(({ title, author, year }: any, id: number) => (
+              <tr key={id}>
+                <td className="px-6 py-4 whitespace-nowrap text-sm">{id + 1}</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm">{title}</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm">{author}</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm">{year}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </Layout >
   );
 };
