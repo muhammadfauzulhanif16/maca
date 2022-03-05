95ebd3774c2d31b4df9fd02e9cc60e0b9c61dcac
muhammadfauzulhanif16 <muhammadfauzulhanif16@outlook.com>
2022-03-05 21:40:25 +0700

    remove var unused

--- client/v2/src/features/common/Layout/index.tsx
+++ client/v2/src/features/common/Layout/index.tsx
@@ -2,13 +2,6 @@ import { FC, ReactNode } from "react";
 import { Header } from "../Header";
 import { Content } from "../Content";
 import { SideBar } from "../SideBar";
-import { useNavigate } from "react-router-dom";
-import {
-  Book,
-  BookAdd,
-  BookOpen,
-  Home,
-} from "@emotion-icons/fluentui-system-regular";
 
 interface LayoutProps {
   titlePage?: string;
@@ -19,26 +12,6 @@ export const Layout: FC<LayoutProps> = ({
   titlePage,
   children,
 }): JSX.Element => {
-  const navigate = useNavigate(),
-    Page = [
-      {
-        icon: <Home width={24} />,
-        title: "Home",
-      },
-      {
-        icon: <BookAdd width={24} />,
-        title: "Add",
-      },
-      {
-        icon: <BookOpen width={24} />,
-        title: "Reading",
-      },
-      {
-        icon: <Book width={24} />,
-        title: "Finished",
-      },
-    ];
-
   return (
     <div className="flex h-screen">
       <SideBar titlePage={titlePage} />
