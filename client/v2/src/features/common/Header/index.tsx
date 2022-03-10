9c2b6d4c637c30e79f626be277775dbe4db43cbb
muhammadfauzulhanif16 <muhammadfauzulhanif16@outlook.com>
2022-03-10 19:49:58 +0700

    add logo & name app in mobile view

--- client/v2/src/features/common/Header/index.tsx
+++ client/v2/src/features/common/Header/index.tsx
@@ -1,11 +1,14 @@
 import { FC } from "react";
-import { Person } from "@emotion-icons/fluentui-system-regular";
+import { Notebook, Person } from "@emotion-icons/fluentui-system-regular";
 import { IconButton } from "../IconButton";
+import { useNavigate } from "react-router-dom";
 
 export const Header: FC<{}> = (): JSX.Element => {
+  const navigate = useNavigate();
+
   return (
     <header>
-      <div className="py-4 lg:py-8 border-b-2 border-zinc-100 border-zinc-800 flex justify-end">
+      <div className="py-4 lg:py-8 border-b-2 border-zinc-100 border-zinc-800 flex justify-between lg:justify-end">
         {/* <label
           className="mr-4 transition-all ease-in-out hover:shadow-sm group flex items-center cursor-pointer rounded-xl hover:bg-zinc-100 dark:hover:bg-zinc-800 px-4 py-2"
           htmlFor="search"
@@ -25,6 +28,16 @@ export const Header: FC<{}> = (): JSX.Element => {
 
           <p className="hidden sm:inline mx-4">Hello, Welcome!</p>
         </div> */}
+
+        <IconButton
+          className="cursor-pointer px-3 text-cyan-400 dark:text-cyan-500 flex lg:hidden items-center font-bold text-2xl"
+          icon={<Notebook width={32} />}
+          onClick={() => {
+            navigate("/");
+          }}
+          text="Maca"
+          textClass="ml-4"
+        />
 
         <IconButton
           className="transition-all ease-in-out hover:shadow-sm flex rounded-xl hover:bg-zinc-100 dark:hover:bg-zinc-800 px-4 py-2 cursor-pointer"
